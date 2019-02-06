deba4e64d3ba5966e236c46ff5e97e7ece1f35bd
Jami Kettunen <47358222+JamiKettunen@users.noreply.github.com>
2019-02-06 20:41:44 +0200

    Add project files

---
+++ PortCheck/MainForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PortCheck
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.picPortUL = new System.Windows.Forms.PictureBox();
+            this.workerUpdateIP = new System.ComponentModel.BackgroundWorker();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblStatusText = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.workerPortChecker = new System.ComponentModel.BackgroundWorker();
+            this.timerResetTitle = new System.Windows.Forms.Timer(this.components);
+            this.txtEditIP = new System.Windows.Forms.TextBox();
+            this.lblEditIP = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPortUL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPort
+            // 
+            this.lblPort.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPort.AutoSize = true;
+            this.lblPort.ForeColor = System.Drawing.Color.White;
+            this.lblPort.Location = new System.Drawing.Point(18, 35);
+            this.lblPort.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(41, 21);
+            this.lblPort.TabIndex = 99;
+            this.lblPort.Text = "Port:";
+            this.lblPort.UseMnemonic = false;
+            this.lblPort.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPort.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtPort.ForeColor = System.Drawing.Color.White;
+            this.txtPort.Location = new System.Drawing.Point(59, 35);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPort.MaxLength = 5;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(65, 22);
+            this.txtPort.TabIndex = 0;
+            this.txtPort.Text = "8080";
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPort.WordWrap = false;
+            this.txtPort.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPort_KeyDown);
+            // 
+            // picPortUL
+            // 
+            this.picPortUL.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picPortUL.BackColor = System.Drawing.Color.White;
+            this.picPortUL.Location = new System.Drawing.Point(59, 57);
+            this.picPortUL.Name = "picPortUL";
+            this.picPortUL.Size = new System.Drawing.Size(65, 1);
+            this.picPortUL.TabIndex = 2;
+            this.picPortUL.TabStop = false;
+            // 
+            // workerUpdateIP
+            // 
+            this.workerUpdateIP.WorkerSupportsCancellation = true;
+            this.workerUpdateIP.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerUpdateIP_DoWork);
+            this.workerUpdateIP.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerUpdateIP_RunWorkerCompleted);
+            // 
+            // lblIP
+            // 
+            this.lblIP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblIP.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.lblIP.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblIP.Location = new System.Drawing.Point(32, 22);
+            this.lblIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(85, 12);
+            this.lblIP.TabIndex = 99;
+            this.lblIP.Text = "IP: X.X.X.X";
+            this.lblIP.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblIP.UseMnemonic = false;
+            this.lblIP.DoubleClick += new System.EventHandler(this.lblIP_DoubleClick);
+            this.lblIP.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // lblStatusText
+            // 
+            this.lblStatusText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblStatusText.AutoSize = true;
+            this.lblStatusText.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.lblStatusText.ForeColor = System.Drawing.Color.LightGray;
+            this.lblStatusText.Location = new System.Drawing.Point(29, 61);
+            this.lblStatusText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatusText.Name = "lblStatusText";
+            this.lblStatusText.Size = new System.Drawing.Size(38, 12);
+            this.lblStatusText.TabIndex = 99;
+            this.lblStatusText.Text = "STATUS:";
+            this.lblStatusText.UseMnemonic = false;
+            this.lblStatusText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.lblStatus.ForeColor = System.Drawing.Color.DarkGray;
+            this.lblStatus.Location = new System.Drawing.Point(65, 61);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(56, 12);
+            this.lblStatus.TabIndex = 99;
+            this.lblStatus.Text = "UNKNOWN";
+            this.lblStatus.UseMnemonic = false;
+            this.lblStatus.SizeChanged += new System.EventHandler(this.lblStatus_SizeChanged);
+            this.lblStatus.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // workerPortChecker
+            // 
+            this.workerPortChecker.WorkerSupportsCancellation = true;
+            this.workerPortChecker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerPortChecker_DoWork);
+            this.workerPortChecker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerPortChecker_RunWorkerCompleted);
+            // 
+            // timerResetTitle
+            // 
+            this.timerResetTitle.Interval = 750;
+            this.timerResetTitle.Tick += new System.EventHandler(this.timerResetTitle_Tick);
+            // 
+            // txtEditIP
+            // 
+            this.txtEditIP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.txtEditIP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEditIP.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtEditIP.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.txtEditIP.ForeColor = System.Drawing.Color.White;
+            this.txtEditIP.Location = new System.Drawing.Point(58, 22);
+            this.txtEditIP.MaxLength = 15;
+            this.txtEditIP.Name = "txtEditIP";
+            this.txtEditIP.Size = new System.Drawing.Size(67, 13);
+            this.txtEditIP.TabIndex = 1;
+            this.txtEditIP.Text = "000.000.000.000";
+            this.txtEditIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtEditIP.Visible = false;
+            this.txtEditIP.WordWrap = false;
+            this.txtEditIP.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEditIP_KeyDown);
+            // 
+            // lblEditIP
+            // 
+            this.lblEditIP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblEditIP.AutoSize = true;
+            this.lblEditIP.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.lblEditIP.ForeColor = System.Drawing.Color.White;
+            this.lblEditIP.Location = new System.Drawing.Point(23, 22);
+            this.lblEditIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEditIP.Name = "lblEditIP";
+            this.lblEditIP.Size = new System.Drawing.Size(35, 12);
+            this.lblEditIP.TabIndex = 99;
+            this.lblEditIP.Text = "Edit IP:";
+            this.lblEditIP.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblEditIP.UseMnemonic = false;
+            this.lblEditIP.Visible = false;
+            this.lblEditIP.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(148, 96);
+            this.Controls.Add(this.lblEditIP);
+            this.Controls.Add(this.txtEditIP);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.picPortUL);
+            this.Controls.Add(this.lblStatusText);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblPort);
+            this.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(139, 95);
+            this.Name = "MainForm";
+            this.Opacity = 0.99D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PortCheck";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picPortUL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.PictureBox picPortUL;
+        private System.ComponentModel.BackgroundWorker workerUpdateIP;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblStatusText;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker workerPortChecker;
+        private System.Windows.Forms.Timer timerResetTitle;
+        private System.Windows.Forms.TextBox txtEditIP;
+        private System.Windows.Forms.Label lblEditIP;
+    }
+}
+
---
+++ PortCheck/MainForm.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Media;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using PortCheck.Properties;
+
+namespace PortCheck
+{
+    public partial class MainForm : Form
+    {
+        private int _status = 0;
+        private int Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+
+                switch (_status)
+                {
+                    case 1: // Checking
+                        lblStatus.ForeColor = Color.DeepSkyBlue; // Gold
+                        lblStatus.Text = "CHECKING";
+                        break;
+                    case 2: // Open
+                        lblStatus.ForeColor = Color.Lime;
+                        lblStatus.Text = "OPEN";
+                        break;
+                    case 3: // Closed
+                        lblStatus.ForeColor = Color.Red;
+                        lblStatus.Text = "CLOSED";
+                        break;
+                    default: // Unknown (0)
+                        lblStatus.ForeColor = Color.DarkGray;
+                        lblStatus.Text = "UNKNOWN";
+                        break;
+                }
+            }
+        }
+
+        private bool _ipEditMode = false;
+        private bool IPEditMode
+        {
+            get => _ipEditMode;
+            set
+            {
+                if (!workerPortChecker.IsBusy && !workerUpdateIP.IsBusy)
+                {
+                    _ipEditMode = value;
+
+                    txtEditIP.Text = this.IPv4.ToString();
+                    lblIP.Visible = !_ipEditMode;
+                    lblEditIP.Visible = _ipEditMode;
+                    txtEditIP.Visible = _ipEditMode;
+                    txtPort.TabStop = !_ipEditMode;
+
+                    if (_ipEditMode)
+                    {
+                        txtEditIP.Select();
+                        txtEditIP.Select(0, txtEditIP.Text.Length);
+                    }
+                    else { this.PortLocked = false; }
+                }
+            }
+        }
+
+        private IPAddress _ipv4 = IPAddress.Parse("0.0.0.0");
+        private IPAddress IPv4
+        {
+            get => _ipv4;
+            set
+            {
+                _ipv4 = value;
+                
+                lblIP.Text = "IP: " + IPv4;
+            }
+        }
+
+        private ushort _port = 80;
+        private ushort Port
+        {
+            get => _port;
+            set
+            {
+                if(!workerPortChecker.IsBusy) // !PortLocked
+                {
+                    _port = value;
+
+                    txtPort.Text = _port.ToString();
+                }
+            }
+        }
+
+        private bool PortLocked
+        {
+            get => txtPort.ReadOnly; // txtPort.Enabled
+            set
+            {
+                txtPort.ReadOnly = value;
+
+                txtPort.ForeColor = !txtPort.ReadOnly ? Color.White : Color.FromArgb(109, 109, 109);
+                lblPort.ForeColor = !txtPort.ReadOnly ? Color.White : Color.FromArgb(109, 109, 109);
+                picPortUL.BackColor = !txtPort.ReadOnly ? Color.White : Color.FromArgb(109, 109, 109);
+            }
+        }
+
+        private int _connTimeout = 3000;
+        private int ConnTimeout
+        {
+            get => _connTimeout;
+            set
+            {
+                if (!workerPortChecker.IsBusy)
+                {
+                    _connTimeout = value;
+                    
+                    if (timerResetTitle.Enabled) { timerResetTitle.Stop(); }
+                    this.Text = "Timeout: " + _connTimeout;
+                    timerResetTitle.Start();
+                }
+            }
+        }
+
+        #region MainForm
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Load last values etc.
+            try
+            {
+                this.IPv4 = IPAddress.Parse(Settings.Default.CachedIP);
+                this.Port = Settings.Default.LastPort;
+                this.Status = 0;
+                this.ConnTimeout = Settings.Default.ConnTimeout;
+            }
+            catch { }
+            
+            // Fetch public IP
+            if (!workerUpdateIP.IsBusy && Settings.Default.CachedIP == "0.0.0.0") { lblIP.Text = "IP: FETCHING..."; workerUpdateIP.RunWorkerAsync(); }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Save last values for next application start
+            try
+            {
+                Settings.Default.CachedIP = this._ipv4.ToString();
+                Settings.Default.LastPort = this._port;
+                Settings.Default.ConnTimeout = this.ConnTimeout;
+                Settings.Default.Save();
+            }
+            catch { }
+        }
+        
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            lblPort.Select();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.F1) { ConnTimeout = 100; return true; }
+            else if (keyData == Keys.F2 && ConnTimeout > 100) { ConnTimeout -= 50; return true; }
+            else if (keyData == Keys.F3 && ConnTimeout < 25000) { ConnTimeout += 50; return true; }
+            else if (keyData == Keys.F4) { ConnTimeout = 25000; return true; }
+            else if (keyData == Keys.F5 && !workerUpdateIP.IsBusy) { lblIP.Text = "IP: FETCHING..."; workerUpdateIP.RunWorkerAsync(); return true; }
+            else if (keyData == Keys.Escape && IPEditMode) { IPEditMode = false; return true; }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        #endregion
+
+        #region workerUpdateIP
+
+        private void workerUpdateIP_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try { e.Result = new WebClient().DownloadString("https://api.ipify.org"); } // Get public IPv4 address
+            catch { e.Result = "0.0.0.0"; }
+        }
+
+        private void workerUpdateIP_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            try { this.IPv4 = IPAddress.Parse(e.Result.ToString()); }
+            catch { MessageBox.Show("IP Address parsing failure!", "PortCheck", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        #endregion
+        
+        #region workerPortChecker
+
+        private void workerPortChecker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            e.Result = null;
+
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    Stopwatch watch = Stopwatch.StartNew();
+                    e.Result = client.ConnectAsync(IPv4, Port).Wait(ConnTimeout) ? 1 : 0;
+                    watch.Stop();
+                    Console.WriteLine($"<= workerPortChecker_DoWork(): Connection took {watch.ElapsedMilliseconds}ms w/ result: port {((int)e.Result == 1 ? "open" : "closed")}");
+                    watch = null;
+                }
+            }
+            catch { e.Result = 0; }
+        }
+
+        private void workerPortChecker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Status = (int)e.Result == 1 ? 2 : 3;
+
+            this.PortLocked = false;
+
+            txtPort.Select();
+            txtPort.Select(0, txtPort.Text.Length);
+        }
+
+        #endregion
+
+        #region UI controls
+
+        private void lblIP_DoubleClick(object sender, EventArgs e)
+        {
+            if (!IPEditMode) { this.PortLocked = true; IPEditMode = true; }
+        }
+
+        private void lblStatus_SizeChanged(object sender, EventArgs e)
+        {
+            // Center 'lblStatusText' and 'lblStatus' together on the X axis
+            int deltaX = ((this.Width - (lblStatusText.Width + lblStatus.Width) - 2) / 2) - lblStatusText.Location.X - 5;
+            lblStatusText.Location = new Point(lblStatusText.Location.X + deltaX, lblStatusText.Location.Y);
+            lblStatus.Location = new Point(lblStatusText.Location.X + lblStatusText.Width - 2, lblStatus.Location.Y);
+        }
+
+        private void txtEditIP_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Escape)
+            {
+                e.Handled = e.SuppressKeyPress = true;
+
+                if (e.KeyCode == Keys.Escape) { return; }
+
+                IPAddress.TryParse(txtEditIP.Text, out IPAddress tmpIP);
+
+                if (tmpIP != null)
+                {
+                    this.IPv4 = tmpIP;
+                    IPEditMode = false;
+                }
+                else { txtEditIP.Select(); txtEditIP.Select(0, txtEditIP.Text.Length); SystemSounds.Asterisk.Play(); }
+            }
+        }
+
+        private void txtPort_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter && !workerPortChecker.IsBusy)
+            {
+                e.Handled = e.SuppressKeyPress = true;
+
+                RunPortCheck();
+            }
+            else if(e.KeyCode == Keys.Escape) { e.Handled = e.SuppressKeyPress = true; lblPort.Select(); }
+        }
+
+        #endregion
+
+        #region Functions
+
+        private void RunPortCheck()
+        {
+            ushort.TryParse(txtPort.Text, out ushort tmpPort);
+
+            if (txtPort.Text != tmpPort.ToString()) // Invalid port
+            {
+                txtPort.Select();
+                txtPort.Select(0, txtPort.Text.Length);
+                SystemSounds.Asterisk.Play();
+                return;
+            }
+            else { this.PortLocked = true; lblPort.Select(); Port = tmpPort; }
+
+            Console.WriteLine("Checking port " + Port + " on IP " + IPv4);
+            this.Status = 1;
+
+            workerPortChecker.RunWorkerAsync();
+        }
+
+        #endregion
+
+        #region Other
+        
+        private void timerResetTitle_Tick(object sender, EventArgs e)
+        {
+            timerResetTitle.Stop();
+            this.Text = "PortCheck";
+        }
+        
+        #endregion
+    }
+}
